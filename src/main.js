c767b1f6a02371659554c59fd01f9af2fb96db1f
Sankalpa Acharya <79009768+Sankalpa-Acharya@users.noreply.github.com>
2021-09-09 20:15:16 +0545

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,34 @@
+var typed = new Typed('.for-typedjs', {
+    strings: ['STUDENT', 'HACKER','DEVELOPER'],
+    'typeSpeed':65,
+    'backSpeed':65,
+    'loop':true,
+    fadeOutClass: 'typed-fade-out',
+  });
+
+
+      window.addEventListener('scroll',(e)=>{
+        const nav = document.querySelector('.head-section');
+        if(window.pageYOffset>0){
+          nav.classList.add("add-shadow");
+        }else{
+          nav.classList.remove("add-shadow");
+        }
+      });
+
+
+      $(document).ready(function(){
+        $(".skillset owl-carousel").owlCarousel({
+          'loop':true,
+          'autoplay':true,
+          'dots':true,
+        });
+      });
+
+
+    document.getElementById('blog-btn').addEventListener('click',()=>{
+      window.location.href='https://sankalpa02.medium.com/testing-cookies-worth-500-8fc2310e6d7e?source=user_profile---------1----------------------------'
+    })
+
+
+  localStorage.setItem('maybethis','flag{Told_you_this_was_easy}')
